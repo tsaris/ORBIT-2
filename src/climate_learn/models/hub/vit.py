92004ce5325a37beed312fb4e88ba17021df0ff9
XiaoWang-Github <wangx2@ornl.gov>
2024-12-13 07:00:05 -0500

    add convolutional layer unet decoder. add residual connection

--- src/climate_learn/models/hub/vit.py
+++ src/climate_learn/models/hub/vit.py
@@ -7,7 +7,7 @@ from .utils import register
 import torch
 import torch.nn as nn
 from timm.models.vision_transformer import Block, PatchEmbed, trunc_normal_
-
+from einops import rearrange
 
 @register("vit")
 class VisionTransformer(nn.Module):
@@ -17,6 +17,8 @@ class VisionTransformer(nn.Module):
         in_channels,
         out_channels,
         history,
+        superres_factor = 4,
+        cnn_ratio = 4,
         patch_size=16,
         drop_path=0.1,
         drop_rate=0.1,
@@ -29,6 +31,8 @@ class VisionTransformer(nn.Module):
     ):
         super().__init__()
         self.img_size = img_size
+        self.cnn_ratio = cnn_ratio
+        self.superres_factor = superres_factor
         self.in_channels = in_channels * history
         self.out_channels = out_channels
         self.patch_size = patch_size
@@ -58,10 +62,24 @@ class VisionTransformer(nn.Module):
         )
         self.norm = nn.LayerNorm(embed_dim)
 
+
+
+        self.upscale = nn.Conv2d(in_channels=1, out_channels=cnn_ratio*superres_factor*superres_factor, kernel_size=(3, 3), stride=1, padding=1) 
+        self.downscale = nn.Conv2d(in_channels=cnn_ratio, out_channels=out_channels, kernel_size=(3, 3), stride=1, padding=1) 
+
+
+
+        self.path1 = nn.ModuleList()
+        self.path1.append(nn.Conv2d(in_channels=out_channels, out_channels=cnn_ratio*superres_factor*superres_factor, kernel_size=(3, 3), stride=1, padding=1)) 
+        self.path1.append(nn.PixelShuffle(superres_factor))
+        self.path1.append(nn.Conv2d(in_channels=cnn_ratio, out_channels=out_channels, kernel_size=(3, 3), stride=1, padding=1)) 
+        self.path1 = nn.Sequential(*self.path1)
+
+
+
         self.head = nn.ModuleList()
         for _ in range(decoder_depth):
-#            self.head.append(nn.Linear(embed_dim, embed_dim))
-            self.head.append(nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(3, 3), stride=1, padding=1)) 
+            self.head.append(nn.Linear(embed_dim, embed_dim))
             self.head.append(nn.GELU())
         self.head.append(nn.Linear(embed_dim, out_channels * patch_size**2))
         self.head = nn.Sequential(*self.head)
@@ -86,15 +104,15 @@ class VisionTransformer(nn.Module):
             nn.init.constant_(m.bias, 0)
             nn.init.constant_(m.weight, 1.0)
 
-    def unpatchify(self, x: torch.Tensor):
+    def unpatchify(self, x: torch.Tensor, scaling =1):
         """
         x: (B, L, V * patch_size**2)
         return imgs: (B, V, H, W)
         """
         p = self.patch_size
         c = self.out_channels
-        h = self.img_size[0] // p
-        w = self.img_size[1] // p
+        h = self.img_size[0] * scaling // p
+        w = self.img_size[1] *scaling // p
         assert h * w == x.shape[1]
         x = x.reshape(shape=(x.shape[0], h, w, p, p, c))
         x = torch.einsum("nhwpqc->nchpwq", x)
@@ -105,26 +123,94 @@ class VisionTransformer(nn.Module):
         # x.shape = [B,C,H,W]
         x = self.patch_embed(x)
         # x.shape = [B,num_patches,embed_dim]
+
+        #if torch.distributed.get_rank()==0:
+        #    print("after patch_embed x.shape",x.shape,flush=True)
+
+        path2_result = x.unsqueeze(dim=1)
+
+
+        #if torch.distributed.get_rank()==0:
+        #    print("after unsqueeze path2_result.shape",path2_result.shape,flush=True)
+
+        path2_result = self.upscale(path2_result)
+
+        #if torch.distributed.get_rank()==0:
+        #    print("after upscale path2_result.shape",path2_result.shape,flush=True)
+
+        path2_result = rearrange(path2_result,'n (c r) l h -> n c (r l) h',c=self.cnn_ratio,r=(self.superres_factor*self.superres_factor))
+
+
+        path2_result = self.downscale(path2_result)
+
+
+        #if torch.distributed.get_rank()==0:
+        #    print("after downscale path2_result.shape",path2_result.shape,flush=True)
+
+
+        path2_result = torch.squeeze(path2_result)
+
+
+        #if torch.distributed.get_rank()==0:
+        #    print("after squeeze path2_result.shape",path2_result.shape,flush=True)
+
+
+        #if torch.distributed.get_rank()==0:
+        #    print("after path2 path2_result.shape",path2_result.shape,flush=True)
+
+
         x = x + self.pos_embed
         x = self.pos_drop(x)
         for blk in self.blocks:
             x = blk(x)
         # x.shape = [B,num_patches,embed_dim]
         x = self.norm(x)
-        return x
+        return x, path2_result
 
     def forward(self, x):
         if len(x.shape) == 5:  # x.shape = [B,T,in_channels,H,W]
             x = x.flatten(1, 2)
         # x.shape = [B,T*in_channels,H,W]
-        x = self.forward_encoder(x)
 
-        x = torch.unsqueeze(x, 1)
+ 
+        #if torch.distributed.get_rank()==0:
+        #    print("forward begin x.shape",x.shape,flush=True)
+
+     
+        x, path2_result = self.forward_encoder(x)
+
+ 
+        #if torch.distributed.get_rank()==0:
+        #    print("forward_encoder x.shape",x.shape,"path2_result.shape",path2_result.shape,flush=True)
+
+
+
         # x.shape = [B,num_patches,embed_dim]
         x = self.head(x)
-  
-        x= torch.squeeze(x)
+
+ 
+        #if torch.distributed.get_rank()==0:
+        #    print("after self.head x.shape",x.shape,flush=True)
+
+
+ 
         # x.shape = [B,num_patches,embed_dim]
-        preds = self.unpatchify(x)
+        x = self.unpatchify(x)
+
+ 
+        #if torch.distributed.get_rank()==0:
+        #    print("after unpatchify x.shape",x.shape,flush=True)
+
+ 
+        #if torch.distributed.get_rank()==0:
+        #    print("after path1 self.path1(x).shape",self.path1(x).shape,flush=True)
+
+  
+        #if torch.distributed.get_rank()==0:
+        #    print("after self.unpatchify(self.head(path2_result),scaling=4).shape",self.unpatchify(self.head(path2_result),scaling=4).shape,flush=True)
+
+     
+        preds = self.path1(x) + self.unpatchify(self.head(path2_result),scaling=4)
+ 
         # preds.shape = [B,out_channels,H,W]
         return preds
