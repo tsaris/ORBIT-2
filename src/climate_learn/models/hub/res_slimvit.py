ab2c4d49f53a1fce5b4948b3f674724732385152
XiaoWang-Github <wangx2@ornl.gov>
2025-01-04 18:55:52 -0500

    updated architecture with improved accuracy

--- src/climate_learn/models/hub/res_slimvit.py
+++ src/climate_learn/models/hub/res_slimvit.py
@@ -63,8 +63,20 @@ class Res_Slim_ViT(nn.Module):
         self.norm = nn.LayerNorm(embed_dim)
 
 
+
+        self.out_conv = nn.ModuleList()
+        self.out_conv.append(nn.GELU()) 
+        self.out_conv.append(nn.Linear(self.img_size[1]*superres_factor, self.img_size[1]*superres_factor))
+        self.out_conv = nn.Sequential(*self.out_conv)
+
+
+
+
         self.path2 = nn.ModuleList()
         self.path2.append(nn.Conv2d(in_channels=in_channels, out_channels=cnn_ratio*superres_factor*superres_factor, kernel_size=(3, 3), stride=1, padding=1)) 
+        self.path2.append(nn.GELU())
+        self.path2.append(nn.BatchNorm2d(cnn_ratio*superres_factor*superres_factor))
+        self.path2.append(nn.Conv2d(in_channels=cnn_ratio*superres_factor*superres_factor, out_channels=cnn_ratio*superres_factor*superres_factor, kernel_size=(3, 3), stride=1, padding=1)) 
         self.path2.append(nn.PixelShuffle(superres_factor))
         self.path2.append(nn.Conv2d(in_channels=cnn_ratio, out_channels=out_channels, kernel_size=(3, 3), stride=1, padding=1)) 
         self.path2 = nn.Sequential(*self.path2)
@@ -81,7 +93,7 @@ class Res_Slim_ViT(nn.Module):
 
         self.path1 = nn.Linear(self.img_size[1], self.img_size[1]*superres_factor*superres_factor)
 
-        self.out_linear = nn.Linear(self.img_size[1]*superres_factor, self.img_size[1]*superres_factor)
+        #self.out_linear = nn.Linear(self.img_size[1]*superres_factor, self.img_size[1]*superres_factor)
 
         self.head = nn.ModuleList()
         for _ in range(decoder_depth):
@@ -148,7 +160,7 @@ class Res_Slim_ViT(nn.Module):
         # x.shape = [B,T*in_channels,H,W]
 
  
-        path2_result = x
+        path2_result = self.path2(x)
         
         x = self.forward_encoder(x)
 
@@ -159,7 +171,8 @@ class Res_Slim_ViT(nn.Module):
         # x.shape = [B,num_patches,embed_dim]
         x = self.unpatchify(x)
     
-        preds = rearrange(self.path1(x),"b c h (w s1 s2) -> b c (h s1) (w s2)",s1= self.superres_factor,s2=self.superres_factor) + self.path2(path2_result)
- 
+        preds = rearrange(self.path1(x),"b c h (w s1 s2) -> b c (h s1) (w s2)",s1= self.superres_factor,s2=self.superres_factor) + path2_result
+
+        preds = self.out_conv(preds) 
         # preds.shape = [B,out_channels,H,W]
         return preds
