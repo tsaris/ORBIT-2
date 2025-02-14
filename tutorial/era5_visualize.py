393b015128782e3ca90ce74e40d1b5a1c22d66e8
XiaoWang-Github <wangx2@ornl.gov>
2025-02-14 14:21:43 -0500

    visualization code for era5

--- tutorial/era5_visualize.py
+++ tutorial/era5_visualize.py
@@ -19,7 +19,7 @@ import sys
 import random
 import time
 import numpy as np
-
+import yaml
 
 from climate_learn.data.processing.era5_constants import (
     PRESSURE_LEVEL_VARS,
@@ -53,34 +53,69 @@ torch.distributed.init_process_group('nccl', timeout=timedelta(seconds=7200000),
 
 # assuming we are downscaling geopotential from ERA5
 
-preset = "res_slimvit"
+config_path = sys.argv[1]
+
+if world_rank==0:
+    print("config_path",config_path,flush=True)
+
+conf = yaml.load(open(config_path,'r'),Loader=yaml.FullLoader)
+
+max_epochs=conf['trainer']['max_epochs']
+checkpoint_path = conf['trainer']['checkpoint']
+batch_size = conf['trainer']['batch_size']
+num_workers = conf['trainer']['num_workers']
+buffer_size = conf['trainer']['buffer_size']
+
+pretrain_path = conf['trainer']['pretrain']
+low_res_dir = conf['data']['low_res_dir']
+high_res_dir = conf['data']['high_res_dir']
+preset = conf['model']['preset']
+out_variable = conf['data']['out_variable']
+dict_in_variables = conf['data']['dict_in_variables']
+out_var_dict = conf['data']['out_var_dict']
+
+lr = float(conf['model']['lr'])
+beta_1 = float(conf['model']['beta_1'])
+beta_2 = float(conf['model']['beta_2'])
+weight_decay = float(conf['model']['weight_decay'])
+warmup_epochs =  conf['model']['warmup_epochs']
+warmup_start_lr =  float(conf['model']['warmup_start_lr'])
+eta_min =  float(conf['model']['eta_min'])
+
+superres_mag = conf['model']['superres_mag']
+cnn_ratio = conf['model']['cnn_ratio']
+patch_size =  conf['model']['patch_size']
+embed_dim = conf['model']['embed_dim']
+depth = conf['model']['depth']
+decoder_depth = conf['model']['decoder_depth']
+num_heads = conf['model']['num_heads']
+mlp_ratio = conf['model']['mlp_ratio']
+drop_path = conf['model']['drop_path']
+drop_rate = conf['model']['drop_rate']
+
+
+if world_rank==0:
+    print("max_epochs",max_epochs," ",checkpoint_path," ",pretrain_path," ",low_res_dir," ",high_res_dir,"preset",preset,"out_variable",out_variable,"out_var_dict",out_var_dict,"lr",lr,"beta_1",beta_1,"beta_2",beta_2,"weight_decay",weight_decay,"warmup_epochs",warmup_epochs,"warmup_start_lr",warmup_start_lr,"eta_min",eta_min,"superres_mag",superres_mag,"cnn_ratio",cnn_ratio,"patch_size",patch_size,"embed_dim",embed_dim,"depth",depth,"decoder_depth",decoder_depth,"num_heads",num_heads,"mlp_ratio",mlp_ratio,"drop_path",drop_path,"drop_rate",drop_rate,"batch_size",batch_size,"num_workers",num_workers,"buffer_size",buffer_size,flush=True)
+
+
+model_kwargs = {'superres_mag':superres_mag,'cnn_ratio':cnn_ratio,'patch_size':patch_size,'embed_dim':embed_dim,'depth':depth,'decoder_depth':decoder_depth,'num_heads':num_heads,'mlp_ratio':mlp_ratio,'drop_path':drop_path,'drop_rate':drop_rate}
+
+
+if world_rank==0:
+    print("model_kwargs",model_kwargs,flush=True)
+
+
+if preset!="vit" and preset!="res_slimvit":
+    print("Only supports vit or residual slim vit training.",flush=True)
+    sys.exit("Not vit or res_slimvit architecture")
+
+
 
 # Set up data
-variables = [
-    "land_sea_mask",
-    "orography",
-    "lattitude",
-#    "toa_incident_solar_radiation",
-    "2m_temperature",
-    "10m_u_component_of_wind",
-    "10m_v_component_of_wind",
-    "geopotential",
-    "temperature",
-#    "relative_humidity",
-#    "specific_humidity",
-#    "u_component_of_wind",
-#    "v_component_of_wind",
-]
-out_var_dict = {
-#     "lsm": "land_sea_mask",
-#    "oro": "orography",
-#     "lat2d": "lattitude",
-    "t2m": "2m_temperature",
-#    "z500": "geopotential_500",
-#    "t850": "temperature_850",
-#     "u10": "10m_u_component_of_wind"
 
-}
+variables = dict_in_variables["ERA5"]
+ 
+
 in_vars = []
 for var in variables:
     if var in PRESSURE_LEVEL_VARS:
@@ -92,21 +127,21 @@ for var in variables:
 #load data module
 data_module = cl.data.IterDataModule(
     "downscaling",
-    "/lustre/orion/world-shared/lrn036/jyc/frontier/ClimaX-v2/data/ERA5-1hr-superres/1.0_deg/", 
-    "/lustre/orion/world-shared/lrn036/jyc/frontier/ClimaX-v2/data/ERA5-1hr-superres/0.25_deg/",
+    low_res_dir, 
+    high_res_dir,
     in_vars,
-    out_vars=[out_var_dict["t2m"]],
+    out_vars=[out_var_dict[k] for k in out_variable],
     subsample=1,
-    batch_size=32,
-    buffer_size=500,
-    num_workers=1,
+    batch_size=batch_size,
+    buffer_size=buffer_size,
+    num_workers=num_workers,
 ).to(device)
 
 data_module.setup()
 
 
 # Set up deep learning model
-model, train_loss,val_losses,test_losses,train_transform,val_transforms,test_transforms = cl.load_downscaling_module(device,data_module=data_module, architecture=preset)
+model, train_loss,val_losses,test_losses,train_transform,val_transforms,test_transforms = cl.load_downscaling_module(device,data_module=data_module, architecture=preset,model_kwargs=model_kwargs)
   
 if dist.get_rank()==0:
     print("train_loss",train_loss,"train_transform",train_transform,flush=True)
@@ -120,7 +155,7 @@ denorm = test_transforms[0]
 
 print("denorm is ",denorm,flush=True)
 
-checkpoint_file = "/lustre/orion/nro108/scratch/xf9/checkpoints/climate/ERA5_rank_0_epoch_61.ckpt"
+checkpoint_file = "/lustre/orion/nro108/scratch/xf9/checkpoints/climate/ERA5_rank_0_epoch_0.ckpt"
 
 if os.path.exists(checkpoint_file):
     print("resume from checkpoint was set to True. Checkpoint path found.",flush=True)
