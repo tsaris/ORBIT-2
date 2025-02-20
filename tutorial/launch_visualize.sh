7b2f2efa1dd945e651592d0727704f12a3c86f6e
XiaoWang-Github <wangx2@ornl.gov>
2025-02-20 14:45:22 -0500

    update file name

--- tutorial/launch_visualize.sh
+++ tutorial/launch_visualize.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=7
 #SBATCH -t 00:10:00
-#SBATCH -q debug
+#SBATCH -p batch
 #SBATCH -o flash-%j.out
 #SBATCH -e flash-%j.out
 
@@ -43,8 +43,7 @@ mkdir -p $MIOPEN_USER_DB_PATH
 export OMP_NUM_THREADS=7
 export PYTHONPATH=$PWD:$PYTHONPATH
 
-time srun -n $((SLURM_JOB_NUM_NODES*1)) python ./prism_visualize.py ../configs/prism_prism.yaml
 
-#time srun -n $((SLURM_JOB_NUM_NODES*1)) python ./era5_visualize.py ../configs/era5_era5.yaml
+time srun -n $((SLURM_JOB_NUM_NODES*1)) python ./visualize.py ../configs/era5_era5.yaml
 
 
