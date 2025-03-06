47325310ce775cc1ec977c87564d56d0c2d95160
XiaoWang-Github <wangx2@ornl.gov>
2025-03-06 16:55:41 -0500

    update visualization launch script

--- tutorial/launch_visualize.sh
+++ tutorial/launch_visualize.sh
@@ -24,24 +24,35 @@ module load PrgEnv-gnu
 module load gcc/12.2.0
 module load rocm/6.2.0
 
+
+
 eval "$(/lustre/orion/world-shared/stf218/atsaris/env_test_march/miniconda/bin/conda shell.bash hook)"
 
 conda activate /lustre/orion/lrn036/world-shared/xf9/flash-attention-torch25
 
-export LD_LIBRARY_PATH=/lustre/orion/world-shared/stf218/junqi/climax/rccl-plugin-rocm6/lib/:/opt/rocm-6.2.0/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/lustre/orion/world-shared/stf218/junqi/climax/rccl-plugin-rocm6/lib/:/opt/rocm-6.2.0/lib:$LD_LIBRARY_PATH
 
-
-
+## DDStore and GPTL Timer
+module use -a /lustre/orion/world-shared/lrn036/jyc/frontier/sw/modulefiles
+module load SR_tools
 
 
 export MIOPEN_DISABLE_CACHE=1
 export NCCL_PROTO=Simple
 export MIOPEN_USER_DB_PATH=/tmp/$JOBID
 mkdir -p $MIOPEN_USER_DB_PATH
+export HOSTNAME=$(hostname)
+export PYTHONNOUSERSITE=1
 
 
 export OMP_NUM_THREADS=7
-export PYTHONPATH=$PWD:$PYTHONPATH
+export PYTHONPATH=$PWD/../src:$PYTHONPATH
+
+export ORBIT_USE_DDSTORE=0 ## 1 (enabled) or 0 (disable)
+
+
+
+
 
 
 #time srun -n $((SLURM_JOB_NUM_NODES*1)) python ./visualize.py ../configs/era5_era5.yaml
